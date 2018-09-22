fc29f9b7dfd39a0af8cf0b53b2308d5f7821f29c
Felicity <felicity.nielson@gmail.com>
2018-09-21 18:36:23 -0600

    adding deviation

--- error_and_deviation/errors.py
+++ error_and_deviation/errors.py
@@ -2,9 +2,10 @@
 Understanding data error mathematics and analysis
 
 This script contains...
-Functions for calculating error and deviation between two data lists.
+Functions for calculating error and deviation between two or one data lists.
     All expressions use the standard Python library only, except rmse which also uses numpy.
 Documentation attempting to illuminate the reasoning behind the math.
+Standard Deviation can be calculated specifying form='rmse' and list2=None
 
 Written by Felicty Nielson, mostly for my own intituion, 
 but if you find this useful, then I am happy to oblige.
@@ -25,26 +26,42 @@ def zero_chk(list1):
             print("0 found in list1. MAPE can not divide by 0.")
 
 def float_chk(list1):
-    """In python 2 versions, at least one of the lists will need to be in floats (not ints)
-       to ensure correct calculation.
+    """ In python 2 versions, at least one of the lists will need to be in floats (not ints)
+        to ensure correct calculation.
     """
     for i, j in enumerate(list1):
         list1[i] = float(j)
     return list1
 
-def errors(form, list1, list2, outlim=0):
-    """Function for calculating error between two data lists. Lists must be of same length and 
-       the elements of the first list corresponding to the elements of the second.
-       Args:
-         form (string): Mathematical formula for calculating error.
-                        Availiable formulas are either 'mae', 'mape', 'mse', or 'rmse'
-         list1 (list of floats): The "true" values
-         list2 (list of floats): The "observed" values
-         outlim (float): default 0 equivalent to None. If the difference between two elements 
-                         is greater than the outlier limit, print exception and exclude from 
-                         error calculation.  
-       Returns:
-         err (int): The computed error.
+
+def mean_list(list1, N):
+    """ Helper function calculates mean and returns list of N means
+    """
+    ave = 0
+    for i in list1:
+        ave += i
+    ave /= N
+    lista = [ave for x in range(0,N)]
+    return lista
+
+def errors(form, deviation=False, list1, list2=None, outlim=0):
+    """ Function for calculating error and deviation between two or one data lists. 
+        Lists must be of same length and the elements of the first list corresponding 
+        to the elements of the second.
+        Args:
+          form  (string): Mathematical formula for calculating error.
+                          Availiable formulas are either 'mae', 'mape', 'mse', or 'rmse'
+          deviation  (bool): default False. If specifying only one list, deviation is automatic.
+                             If specifying two lists, deviation of the second list is measured off of 
+                             the mean of the first.
+          list1 (list of floats): The "true" values
+          list2 (list of floats): The "observed" values. Default None. 
+                                  If None, the deviation of list1 is calculated.
+          outlim (float): default 0 equivalent to None. If the difference between two elements 
+                          is greater than the outlier limit, print exception and exclude from 
+                          error calculation.  
+        Returns:
+          err (int): The computed error or deviation.
     """
     import numpy as np
     
@@ -53,6 +70,17 @@ def errors(form, list1, list2, outlim=0):
     outlim_count = 0
     error = 0
     N = len(list1)
+
+
+    # Determine if calculating error or deviation. If calculating deviation, build mean list.
+    if list2 != None and deviation == False:
+        print("Calculating error...")
+    else:
+        print("Calculating deviation...")
+        if list2=None:
+            list2 = mean_list(list1, N)
+        elif deviation = True:
+            list1 = mean_list(list1, N)
     
     for i in range(0, N):
         if form == 'mae':
@@ -68,6 +96,13 @@ def errors(form, list1, list2, outlim=0):
             That is the error between two values. If we have two lists and we want a single value
             to represent the error of all data, we can average them 
             (sum and divide by the total number of errors or data point comparisons).
+
+            To calculate deviation, we modify the formula to subtract the mean of one list from the values
+            of another list. This lets us know how far the observed values are deviating from the true average.
+            Or we can subtract the values of one list from the mean of the same list. This measures how 
+            much the data deviates from its own average.
+            
+            Standard deviation uses RMSE (or rather, RMSD...). See the 'rmse' condition below.
             """
 
             
@@ -116,6 +151,8 @@ def errors(form, list1, list2, outlim=0):
             Root Mean Squared Error (RMSE)
 
             Sqrt(Sum((y - y')^2)/N)
+
+            Specifying only one list will result in the Standard Deviation (SD).
 
             Warning: outliers will dramatically increase the error for MSE and RMSE. 
             Suggest specifying an outlim (outlier limit) value.
